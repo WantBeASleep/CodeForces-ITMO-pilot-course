d02bae57aee74917d91649ecaac715a29e0e8324
Zakhar <mix.cat@mail.ru>
2023-10-01 18:33:34 +0300

    final Z

--- ITMO/Z-func/4.6.cpp
+++ ITMO/Z-func/4.6.cpp
@@ -2,10 +2,12 @@
 
 using namespace std;
 
+vector<int> z(10e6 + 10, 0);
+
 int zfunc(string s, int n) {
-    vector<int> z(s.length(), 0);
     int l = 0, r = 0;
-    for (int i = n + 1; i < s.length(); i++) {
+    for (int i = 1; i < s.length(); i++) {
+        z[i] = 0;
         if (i < r) z[i] = min(z[i - l], r - i);
         while (i + z[i] < s.length() and s[z[i]] == s[i + z[i]]) z[i]++;
         if (z[i] == n) return -1;
@@ -21,9 +23,10 @@ int zfunc(string s, int n) {
 int main()
 {
     ios::sync_with_stdio(NULL), cin.tie(0), cout.tie(0);
-    int glbl;
-    cin >> glbl;
-    for (int gl = 0; gl < glbl; gl++) {
+    int q;
+    cin >> q;
+
+    while (q--) {
         string s, t;
         cin >> s >> t;
 
@@ -39,8 +42,8 @@ int main()
             continue;
         }
 
-        if (st > ts) cout << t + s.substr(st) << "\n";
-        else cout << s + t.substr(ts) << "\n";
+        if (st > ts) cout << t << s.substr(st) << "\n";
+        else cout << s << t.substr(ts) << "\n";
     }
     return 0;
 }

28971848b1728b86c8dccb0d3db785b76f1b6ac4
Zakhar <mix.cat@mail.ru>
2023-11-07 08:19:19 +0300

    ?

--- codeforce pilot course/SegtreePart2/2.4.cpp
+++ codeforce pilot course/SegtreePart2/2.4.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <utility>
-#include <climits>
 
 using namespace std;
 
@@ -27,8 +25,8 @@ class Segtree{
         vector<p> a;
         int size;
 
-        ll op_modify(ll a, ll b) {
-            return a + b;
+        ll op_modify(ll a, ll v) {
+            return a + v;
         }
 
         ll op_calc(ll a, ll b) {
@@ -42,6 +40,7 @@ class Segtree{
         }
 
         void modify(int l, int r, int v, int x, int lx, int rx) {
+            if (x >= 2 * size - 1) return;
             if (lx >= r or rx <= l) return;
             if (lx >= l and rx <= r) {
                 a[x].first = op_modify(a[x].first, v * (rx - lx));
@@ -53,20 +52,26 @@ class Segtree{
             modify(l, r, v, 2 * x + 1, lx, m);
             modify(l, r, v, 2 * x + 2, m, rx);
 
-            a[x].first = op_modify(op_calc(a[2 * x + 1].first, a[2 * x + 2].first), a[x].second * (rx - lx));
+            ll tmp = op_calc(a[2 * x + 1].first, a[2 * x + 2].first);
+            a[x].first = op_modify(tmp, a[x].second * (rx - lx));
+
+            // a[x].first = op_modify(op_calc(a[2 * x + 1].first, a[2 * x + 2].first), a[x].second * (rx - lx));
         }
 
         ll calc(int l, int r, int x, int lx, int rx) {
-            if (lx >= r or rx <= l) return 0;
-            if (lx >= l and rx <= r) {
-                return a[x].first;
-            }
+            if (x >= 2 * size - 1) return 0;
+            if (lx >= r or rx <= l) return (ll)0;
+            if (lx >= l and rx <= r) return a[x].first;
 
             int m = (lx + rx) / 2;
             ll left = calc(l, r, 2 * x + 1, lx, m);
             ll right = calc(l, r, 2 * x + 2, m, rx);
 
-            return op_modify(op_calc(left, right), a[x].second * (min(r, rx) - max(l, lx)));
+            ll tmp = op_calc(left, right);
+            ll res = op_modify(tmp, a[x].second * (min(r, rx) - max(l, lx)));
+
+            // return op_modify(op_calc(left, right), a[x].second * (min(r, rx) - max(l, lx)));
+            return res;
         }
 
 };

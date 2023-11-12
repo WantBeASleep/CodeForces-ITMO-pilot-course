3d9df5e4a422de9c6041e09f1c35d278a77a7680
zakhar <mix.cat@mail.ru>
2023-11-12 16:28:57 +0300

    y

--- YandexCourse/3lesson/C++/D.cpp
+++ YandexCourse/3lesson/C++/D.cpp
@@ -4,7 +4,6 @@
 #include <sstream>
 #include <string>
 
-
 using namespace std;
 
 int main()
@@ -12,7 +11,12 @@ int main()
     ios::sync_with_stdio(NULL), cin.tie(0), cout.tie(0);
     int n;
     cin >> n;
+    unordered_set<int> yes;
     unordered_set<int> no;
+
+    // после ввода числа getline говна поест и нагнется
+    string skipendl;
+    getline(cin, skipendl);
 
     for (;;) {
         string str, status;
@@ -24,15 +28,19 @@ int main()
 
         istringstream stream(str);
         int x;
-        if (status == "NO") {
-            while (stream >> x) {
-                no.insert(x);
-            }
+
+        while (stream >> x) {
+            if (status == "YES" and (x <= n)) yes.insert(x);
+            else if (status == "NO" and (x <= n)) no.insert(x);
         }
     }
 
+    for (auto& x : no) {
+        if (yes.count(x)) yes.erase(x);
+    }
+
     for (int i = 1; i != n + 1; ++i) {
-        if (!no.count(i)) cout << i << " ";
+        if (yes.count(i)) cout << i << " ";
     }
 
     cout << endl;
---
+++ YandexCourse/3lesson/Go/D/D.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strings"
+	"strconv"
+	"fmt"
+)
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	ans := make(map[int]int)
+
+	for i := 1; i != n + 1; i++ {
+		ans[i] = 1
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		line, _ := reader.ReadString('\n')
+		if line == "HELP" {
+			break;
+		}
+		status, _ := reader.ReadString('\n')
+
+		strlist := strings.Split(line[:len(line) - 1], " ")
+		
+		if status == "YES" {
+			newmap := make(map[int]int)
+			for _, s := range strlist {
+				num, _ := strconv.Atoi(s)
+				_, flag := ans[num]
+				if flag {
+					newmap[num] = 1
+				}
+			}
+
+			ans = newmap
+		} else {
+			for _, s := range strlist {
+				num, _ := strconv.Atoi(s)
+				_, flag := ans[num]
+				if flag {
+					delete(ans, num)
+				}
+			}
+		}
+	}
+
+
+	for num := range ans {
+		
+	}
+}
+

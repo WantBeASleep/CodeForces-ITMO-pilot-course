7cf33dd2125438e58b0f6badf318e22c29641956
zakhar <mix.cat@mail.ru>
2023-11-27 16:48:01 +0300

    suka

---
+++ codeforce pilot course/binSearh/Go/4.2/4.2.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	// "fmt"
+	"bufio"
+	"os"
+)
+
+/*
+задача гавно на пару часов, может когда нибудь руки дойдут, но не сегодня
+*/
+
+var cin = bufio.NewReader(os.Stdin)
+var cout = bufio.NewWriter(os.Stdout)
+
+func main() {
+
+	cout.Flush()
+}
